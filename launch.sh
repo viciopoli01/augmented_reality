76d87067449ea4d10a5921498555f04805d3e3e9
Vincenzo Polizzi <polivicio@gmail.com>
2019-10-31 19:16:26 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch augmented_reality augmented_reality.launch map_file:=hub.yaml  robot_name:=cristina
+#roslaunch augmented_reality augmented_reality.launch map_file:=hub.yaml  robot_name:=DUCKIEBOT_NAME
